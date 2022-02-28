7796edf86b92a69f50c391027a201a9847f44bc8
qowldud95 <girlwldud@naver.com>
2022-02-28 22:41:51 +0900

    test

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AdvancedApplication {
 
 	public static void main(String[] args) {
+		//s
 		SpringApplication.run(AdvancedApplication.class, args);
 	}
 
